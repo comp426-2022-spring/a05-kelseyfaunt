ae9b1735674091963bc638013fbb9cb332943c06
kelseyfaunt <59808596+kelseyfaunt@users.noreply.github.com>
2022-05-01 22:13:08 -0400

    first attempt

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,22 @@
 // Put your database code here
+"use strict";
+
+const database = require('better-sqlite3')
+const logdb = new database('log.db')
+
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
+);
+
+let row = stmt.get();
+if(row == undefined) {
+    console.log('Your database appears to be empty. I will initialize it now.');
+
+     const sqlInit = `
+    CREATE TABLE accesslog (id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url TEXT, protocol TEXT, status TEXT, httpversion TEXT, referer TEXT, useragent TEXT );`
+    logdb.exec(sqlInit)
+} else {
+    console.log('Database exists')
+}
+
+
+module.exports = logdb
